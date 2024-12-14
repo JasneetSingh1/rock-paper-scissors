7049844226b46a62fa1d4923cae85968dd10a56e
Jasneet Singh <100010929+JasneetSingh1@users.noreply.github.com>
2024-12-14 15:33:02 -0700

    Develop function to play one round of rock paper scissors

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(){
 
     try{
@@ -26,7 +29,6 @@ function getHumanChoice(){
 
     try{
         let result = prompt("Enter rock, paper, or scissors");
-        console.log(typeof(result));
 
         while(result !== "rock" && result !== "scissors" && result !== "paper"){
             alert("Invalid input");
@@ -38,4 +40,39 @@ function getHumanChoice(){
     }catch(e){}
 }
 
-console.log(getHumanChoice())
+function playRound(humanChoice, computerChoice){
+    let human = humanChoice;
+    let computer = computerChoice;
+
+    human.toLowerCase();
+
+    if(human == "rock" && computer == "scissors"){
+        console.log("You win! Rock beats Scissors");
+        humanScore++;
+    }
+    else if(human == "rock" && computer == "paper"){
+        console.log("You loose! Paper beats Rock");
+        computerScore++;
+    }
+    else if(human == "paper" && computer == "rock"){
+        console.log("You win! Paper beats Rock");
+        humanScore++;
+    }
+    else if(human == "paper" && computer == "scissors"){
+        console.log("You loose! Scissors beats Paper");
+        computerScore++;
+    }
+    else if(human == "scissors" && computer == "paper"){
+        console.log("You win! Scissors beats Paper");
+        humanScore++;
+    }
+    else if(human == "scissors" && computer == "rock"){
+        console.log("You loose! Rock beats Scissors");
+        computerScore++;
+    }
+    else{
+        console.log("You Tied!");
+    }
+}
+
+

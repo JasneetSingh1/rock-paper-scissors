193d09ba528c9091fb34ffb9390e62a89b9fb002
Jasneet <jasneetpalsingh619@gmail.com>
2024-12-19 18:24:43 -0700

    Develop UI to play game
    Created buttons, and modified DOM to dynamically
    display scores, and game status to user

--- index.js
+++ index.js
@@ -1,25 +1,42 @@
 var humanScore = 0;
 var computerScore = 0;
 
-function playGame(playRound){
-    try{
-        for(let i = 0; i < 5; i++){
-            playRound(getHumanChoice(), getComputerChoice());
-            console.log("Your score: " + humanScore + "\n" + "Opponent's score: " + computerScore);
-        }
-    }catch(e){}
 
-    console.log("GAME OVER !!");
-    
-    if(humanScore > computerScore){
-        console.log("YOU WIN !!");
-    }else if(humanScore < computerScore){
-        console.log("YOU LOOSE !!");
-    }else{
-        console.log("YOU TIED !!")
-    }
-    
-}
+
+const paper = document.createElement('button');
+paper.textContent = 'PAPER';
+paper.classList.toggle('paper');
+
+const rock = document.createElement('button');
+rock.textContent = 'ROCK';
+rock.classList.toggle('rock');
+
+const scissors = document.createElement('button');
+scissors.textContent = 'SCISSORS';
+scissors.classList.toggle('scissors');
+
+const container = document.createElement('div');
+container.classList.toggle('container');
+
+const body = document.querySelector('body');
+body.appendChild(container);
+
+container.appendChild(paper)
+container.appendChild(rock)
+container.appendChild(scissors)
+
+
+
+
+
+const score = document.createElement('div');
+score.classList.toggle('score');
+score.textContent = `SCORE: YOU ${humanScore}  VS  OPP ${computerScore}`;
+body.appendChild(score);
+
+const result = document.createElement('div');
+result.classList.toggle('result');
+body.appendChild(result);
 
 function getComputerChoice(){
 
@@ -38,63 +55,90 @@ function getComputerChoice(){
             return "scissors";
     
         }
-    
         
     }catch(e){}
     
-        
 }
 
-function getHumanChoice(){
-
-    try{
-        let result = prompt("Enter rock, paper, or scissors");
-
-        while(result !== "rock" && result !== "scissors" && result !== "paper"){
-            alert("Invalid input");
-            result = prompt("Enter only rock, paper, or scissors");
-        }
-
-        return result;
-        
-    }catch(e){}
-}
 
 function playRound(humanChoice, computerChoice){
     let human = humanChoice;
     let computer = computerChoice;
 
     human.toLowerCase();
-
+    
     if(human == "rock" && computer == "scissors"){
-        console.log("You win! Rock beats Scissors");
+        result.textContent = "You win! Rock beats Scissors";
+        
         humanScore++;
+
     }
     else if(human == "rock" && computer == "paper"){
-        console.log("You loose! Paper beats Rock");
+        result.textContent = "You loose! Paper beats Rock";
         computerScore++;
     }
     else if(human == "paper" && computer == "rock"){
-        console.log("You win! Paper beats Rock");
+        result.textContent = "You win! Paper beats Rock";
+       
         humanScore++;
     }
     else if(human == "paper" && computer == "scissors"){
-        console.log("You loose! Scissors beats Paper");
+        result.textContent = "You loose! Scissors beats Paper";
+        
         computerScore++;
     }
     else if(human == "scissors" && computer == "paper"){
-        console.log("You win! Scissors beats Paper");
+        result.textContent = "You win! Scissors beats Paper";
+       
         humanScore++;
     }
     else if(human == "scissors" && computer == "rock"){
-        console.log("You loose! Rock beats Scissors");
+        result.textContent = "You loose! Rock beats Scissors";
+       
         computerScore++;
     }
     else{
-        console.log("You Tied!");
+        result.textContent = "You Tied!";
+       
     }
 }
 
+container.addEventListener('click', function(e){
+    let button = e.target.classList.value;
+    
+    switch(button){
+
+        case 'scissors':
+            playRound(button, getComputerChoice());
+            break;
+        case 'rock':
+            playRound(button, getComputerChoice());
+            break;
+
+        case 'paper':
+            playRound(button, getComputerChoice());
+            break;
+        
+        default:
+            alert("Invalid input");
+            break;
+    }
+
+    score.textContent = `SCORE: YOU ${humanScore}  VS  OPP ${computerScore}`;
+
+    if(humanScore == 5){
+        result.textContent = "GAME OVER!!\nYOU WIN!!";
+        humanScore = 0;
+        computerScore = 0;
+        score.textContent = `SCORE: YOU ${humanScore}  VS  OPP ${computerScore}`;
+    }else if(computerScore == 5){
+        result.textContent = "GAME OVER!!\nYOU LOOSE!!";
+        humanScore = 0;
+        computerScore = 0;
+        score.textContent = `SCORE: YOU ${humanScore}  VS  OPP ${computerScore}`;
+    }
 
 
-playGame(playRound);
+})
+
+
